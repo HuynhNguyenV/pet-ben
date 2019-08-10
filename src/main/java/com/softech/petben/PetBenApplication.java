dae190b77f070621e3bd880d12389ba383aa61e3
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-08-10 21:52:32 +0700

    the first commit

---
+++ src/main/java/com/softech/petben/PetBenApplication.java
@@ -0,0 +1,13 @@
+package com.softech.petben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softech/petben/PetBenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softech.petben;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetBenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
